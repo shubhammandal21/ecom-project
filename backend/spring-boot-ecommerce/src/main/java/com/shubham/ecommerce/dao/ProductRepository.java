1dc769628263bb7a4403858afcc0e33c665076d4
Shubham Mandal <smshubham238@gmail.com>
2024-01-04 15:26:24 +0530

    Added angular routes for different categories

--- backend/spring-boot-ecommerce/src/main/java/com/shubham/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/shubham/ecommerce/dao/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.shubham.ecommerce.dao;
 
 import com.shubham.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
@@ -9,4 +12,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 public interface ProductRepository extends JpaRepository <Product, Long> {
 
+    Page<Product> findByCategoryId(@Param("id")Long id,Pageable pageable);
 }
--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list.component.html',
-  //templateUrl: './product-list-table.component.html',
+
   templateUrl: './product-list-grid.component.html',
 
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  constructor(private productService: ProductService) {}
-
   products: Product[] = [];
+  currentCategoryId: number = 1;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
   listProducts() {
-    this.productService.getProductList().subscribe((data) => {
-      this.products = data;
-    });
+    //check if "id" parameter is available
+
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      //get the 'id' param string . convert string to a number using the + symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      //not category id available ....default to categoty 1
+      this.currentCategoryId = 1;
+    }
+
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
